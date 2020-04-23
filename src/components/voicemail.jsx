9f33293f5d3b7850c85bc3ed8bd0a20425acc018
Mego1596 <maurigranados1596@gmail.com>
2020-04-22 19:51:45 -0600

    Milestone 2 done
    -Form creation to update the status of the messages of an specific voicemailbox

--- src/components/voicemail.jsx
+++ src/components/voicemail.jsx
@@ -2,15 +2,6 @@ import React, { Component } from "react";
 
 import VoicemailCard from "./voicemailcard";
 import Axios from "axios";
-const serverUrl = "https://sandbox.2600hz.com:8443/v2";
-const credentials =
-  "NDY0MmU2NDA0MGNkYjhiODljMzEwYTIxYTA3YzdmNjI6MjMyNjQxNTY1OTA3NWU3NTAwMGNlY2Q3YmNiZjM3NTY=";
-const accountId = "4642e64040cdb8b89c310a21a07c7f62";
-const vmBoxId = "b37675a2d7b90d60f0ee5d4175502394";
-const listVoicemail = `/accounts/${accountId}/vmboxes/${vmBoxId}/messages/`;
-const headers = {
-  Authorization: `Basic ${credentials}`,
-};
 class Voicemail extends Component {
   state = {
     voicemails: [],
@@ -26,29 +17,17 @@ class Voicemail extends Component {
     voicemail.folder = e.currentTarget.status.value;
     vmBoxes[index] = voicemail;
     this.setState({ voicemails: vmBoxes });
-    /*const { data } = await Axios.post(
-      "https://cors-anywhere.herokuapp.com/" +
-        serverUrl +
-        listVoicemail +
-        vmBoxes[index].media_id,
-      {
-        params: {
-          folder: vmBoxes[index].folder,
-        },
-        headers,
-      }
-    );*/
+    const data = await Axios.get(
+      `http://localhost:3001/voicemailMessage/${vmBoxes[index].media_id}/${vmBoxes[index].folder}`
+    );
     status.removeAttribute("disabled");
   };
 
   async componentDidMount() {
-    const {
-      data: voicemails,
-    } = await Axios.get(
-      "https://cors-anywhere.herokuapp.com/" + serverUrl + listVoicemail,
-      { headers }
+    const { data: voicemails } = await Axios.get(
+      "http://localhost:3001/listVoicemail"
     );
-    const vmBox = voicemails.data;
+    const vmBox = voicemails;
     this.setState({ voicemails: vmBox });
   }
   render() {
